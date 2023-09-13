b863ff143300c95c3b6864bc54b189efdbfb1fe3
Polina Ryabova <polinaryabova.2@gmail.com>
2023-09-13 23:41:32 +0500

    create API interactions

---
+++ client/src/API/plantAPI.js
@@ -0,0 +1,33 @@
+import { $authHost, $host } from './index';
+
+export const createType = async (type) => {
+  const { data } = await $authHost.post('api/type', type);
+  return data;
+};
+
+export const fetchTypes = async () => {
+  const { data } = await $host.get('api/type');
+  return data;
+};
+
+export const createPlant = async (device) => {
+  const { data } = await $authHost.post('api/plant', device);
+  return data;
+};
+
+export const fetchPlants = async (typeId, brandId, page, limit = 5) => {
+  const { data } = await $host.get('api/plant', {
+    params: {
+      typeId,
+      brandId,
+      page,
+      limit,
+    },
+  });
+  return data;
+};
+
+export const fetchOnePlant = async (id) => {
+  const { data } = await $host.get('api/plant/' + id);
+  return data;
+};
