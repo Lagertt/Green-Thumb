6cd0414711948395e6ca96db983ca03b5c875252
Polina Ryabova <polinaryabova.2@gmail.com>
2023-09-13 23:42:30 +0500

    create context provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.scss';
+import './styles/index.scss';
+import UserStore from './store/UserStore';
+import PlantStore from './store/PlantStore';
+
+export const Context = createContext(null);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Context.Provider
+      value={{
+        user: new UserStore(),
+        device: new PlantStore(),
+      }}
+    >
+      <App />
+    </Context.Provider>
   </React.StrictMode>
 );
