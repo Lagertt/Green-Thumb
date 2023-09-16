36943c0193c924ef9900f25d724ecb86a90d1144
Polina Ryabova <polinaryabova.2@gmail.com>
2023-09-16 21:53:35 +0500

    create hot news section

---
+++ client/src/components/HotNews/HotNews.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import cl from './HotNews.module.scss';
+import Container from '../Container/Container';
+import SectionTitle from '../SectionTitle/SectionTitle';
+import PlantCard from '../PlantCard/PlantCard';
+
+function HotNews() {
+  return (
+    <section className={cl.container}>
+      <Container>
+        <SectionTitle title="Горячие новинки" haveButton={true}></SectionTitle>
+        <div className={cl.cards}>
+          <PlantCard name="Neon Photos" price="350"></PlantCard>
+          <PlantCard name="Neon Photos" price="350"></PlantCard>
+          <PlantCard name="Neon Photos" price="350"></PlantCard>
+          <PlantCard name="Neon Photos" price="350"></PlantCard>
+          <PlantCard name="Neon Photos" price="350"></PlantCard>
+          <PlantCard name="Neon Photos" price="350"></PlantCard>
+          <PlantCard name="Neon Photos" price="350"></PlantCard>
+          <PlantCard name="Neon Photos" price="350"></PlantCard>
+        </div>
+      </Container>
+    </section>
+  );
+}
+
+export default HotNews;
