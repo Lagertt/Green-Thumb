191ee1f959728f8e2c2126e01560a9cbffa0b49b
Polina Ryabova <polinaryabova.2@gmail.com>
2023-09-16 21:51:58 +0500

    create hero component

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import cl from './Hero.module.scss';
+import Button from '../../components/Button/Button';
+
+function Hero() {
+  return (
+    <section className={cl.hero}>
+      <div className={cl.wrapper}>
+        <h1 className={cl.title}>Укрась свой дом зеленью от нас!</h1>
+        <p className={cl.descr}>Даже если вы не опытный садовод, вы сможете озеленить свой дом</p>
+        <Button light={true}>Приступить к посадке</Button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
